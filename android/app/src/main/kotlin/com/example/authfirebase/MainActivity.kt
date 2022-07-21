a62cb31ee67a4e59665d89d81301a3dbc703d28a
himanshu571 <77039741+himanshu571@users.noreply.github.com>
2022-07-21 15:06:03 +0530

    user athentication

---
+++ android/app/src/main/kotlin/com/example/authfirebase/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.authfirebase
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,24 @@
+import 'package:authfirebase/screens/login.dart';
+import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+
+Future main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'firebase-auth',
+      theme: ThemeData(
+        primarySwatch: Colors.red,
+      ),
+      debugShowCheckedModeBanner: false,
+      home: LoginScreen(),
+    );
+  }
+}
